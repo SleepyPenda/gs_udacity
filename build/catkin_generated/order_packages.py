713e56f1377fb403da3c3f521661787c6e300640
Giseon Sung <giseon.sung@alumni.stonybrook.edu>
2023-01-11 23:13:41 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/giseon/udacity_gs/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/giseon/scout_ws/devel;/opt/ros/noetic'.split(';') if '/home/giseon/scout_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/giseon/udacity_gs/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/giseon/udacity_gs/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/giseon/udacity_gs/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/giseon/udacity_gs/src:$ROS_PACKAGE_PATH"
