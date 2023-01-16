fd8e9f56f77730bc91a12086df133e5fc0003a23
Giseon Sung <giseon.sung@alumni.stonybrook.edu>
2023-01-16 15:12:16 +0900

    project2 added

---
+++ project2/build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/giseon/udacity_gs/project2/src/my_robot
+# Build directory: /home/giseon/udacity_gs/project2/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
