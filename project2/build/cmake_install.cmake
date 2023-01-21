a422b7c81d71ab7e1a56f1e67830da2985b42e57
Giseon Sung <giseon.sung@alumni.stonybrook.edu>
2023-01-22 00:17:33 +0900

    ball chaser

--- project2/build/cmake_install.cmake
+++ project2/build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/giseon/udacity_gs/project2/build/gtest/cmake_install.cmake")
   include("/home/giseon/udacity_gs/project2/build/my_robot/cmake_install.cmake")
+  include("/home/giseon/udacity_gs/project2/build/ball_chaser/cmake_install.cmake")
 
 endif()
 
